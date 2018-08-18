8470d32cdda6f8ce5974b0c59ac5b017dfbc4de0
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-08-18 04:28:34 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -12,6 +12,12 @@ def countdown(sec)
 end
 
 def countdown_with_sleep(sec)
-  countdown(sec)
+  while sec>0
+    puts "#{sec} SECOND(S)!"
+    sec -= 1
+  end
   
+  if sec == 0 
+    return "HAPPY NEW YEAR!"
+  end
 end
